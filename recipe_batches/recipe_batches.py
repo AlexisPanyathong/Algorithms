5f3242adc3849b9af152e35fc3ab35c760a6cb8e
Alexis Panyathong <alexispanyathong@Alexiss-Air.lan>
2020-01-22 17:56:18 -0600

    Progress:
    I completed the recipe_batches.py and tested it and it passed.

--- recipe_batches/recipe_batches.py
+++ recipe_batches/recipe_batches.py
@@ -3,7 +3,25 @@
 import math
 
 def recipe_batches(recipe, ingredients):
-  pass 
+  # Create an empty list(array)
+  potent_batches = []
+  
+  # Create a for loop of an item in recipe
+  for item in recipe:
+    # If an item is not in the ingredients return 0(none)
+    if item not in ingredients:
+        return 0
+    # If the item in ingredients is divided by the item in recipes is greater than 0.
+    if ingredients[item] // recipe[item] > 0:
+      # Append potent_batches with the ingredients[item] // recipe[item], if order to find the amount we need for the recipe.
+      potent_batches.append(ingredients[item] // recipe[item])
+      
+    # Otherwise return 0
+    else:
+      return 0
+ 
+  # We need to find out how many batches can be made, hence the min.
+  return min(potent_batches)     
 
 
 if __name__ == '__main__':
